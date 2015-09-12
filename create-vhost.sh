19791640957a0f7c3ce2e910a3d0b6d2ed9bab6c
shone <shonezlo@gmail.com>
2015-09-12 12:30:05 +0200

    Script for creating virtual host

---
+++ create-vhost.sh
@@ -0,0 +1,68 @@
+script_title="Create a virtual host"
+server_name=""
+doc_root_path=""
+log_dir_path=""
+
+# pickup server name
+dialog --title "Server name (domain)" --backtitle "$script_title" --inputbox "" 8 60 2>/tmp/input.$$
+
+sel=$?
+
+case $sel in
+	0)	server_name=`cat /tmp/input.$$` ;;
+	*)	clear
+		echo Aborted 
+		exit ;;
+esac
+
+
+
+# pickup doc root path
+dialog --title "Document root path" --backtitle "$script_title" --inputbox "" 8 60 2>/tmp/input.$$
+
+sel=$?
+
+case $sel in
+	0)	doc_root_path=`cat /tmp/input.$$` ;;
+	*)	clear
+		echo Aborted 
+		exit ;;
+esac
+
+
+
+# pickup log path
+dialog --title "Log directory path" --backtitle "$script_title" --inputbox "If not set, defaults to APACHE_LOG_DIR" 8 60 2>/tmp/input.$$
+
+sel=$?
+
+case $sel in
+	0)	log_dir_path=`cat /tmp/input.$$` ;;
+	*)	clear
+		echo Aborted 
+		exit ;;
+esac
+
+if [ -z "$log_dir_path" ]
+then
+	log_dir_path='${APACHE_LOG_DIR}'
+fi
+
+vhost="<VirtualHost *:80>\n"
+vhost="$vhost\tServerName $server_name\n"
+vhost="$vhost\tServerAdmin webmaster@localhost\n"
+vhost="$vhost\tDocumentRoot $doc_root_path\n\n"
+vhost="$vhost\tErrorLog $log_dir_path/error.log\n"
+vhost="$vhost\tCustomLog $log_dir_path/access.log combined\n\n"
+vhost="$vhost\t<Directory $doc_root_path>\n"
+vhost="$vhost\t\tAllowOverride All\n"
+vhost="$vhost\t\tRequire all granted\n"
+vhost="$vhost\t</Directory>\n"
+vhost="$vhost</VirtualHost>"
+
+if [ -f vhost.conf ]; then
+	rm vhost.conf
+fi
+
+clear
+echo -e "$vhost" >> vhost.conf
